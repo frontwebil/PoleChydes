f0fde872941201e2b8c45efe04c050d3ccf3d43b
Ілюха <152032402+frontwebil@users.noreply.github.com>
2024-03-09 15:24:13 +0300

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,51 @@
+let words = [
+  "небо",
+  "коло",
+  "чоло",
+  "раки"
+];
+
+let slovo = words[Math.floor(Math.random()*4)];
+console.log(slovo);
+
+let answer = ["*","*","*","*"];
+
+let remain = slovo.length;
+
+while (remain>0) {
+
+  alert(answer.join(" "));
+
+  let guess = prompt("Введіть букву");
+
+  guess = guess.toLowerCase();
+
+
+
+
+  if (guess === null) {
+    break;
+  }
+
+  else if (guess.length !==1) {
+    alert("Введіть ОДНУ букву!")
+
+  }
+  else {
+
+    for (let i = 0 ; i < slovo.length; i++){
+
+      if (slovo[i] === guess) {
+
+        answer[i] = guess;
+        remain--;
+
+      }
+
+    }
+
+  }
+
+}
+
+alert("Ви вгадали слово" , slovo);
